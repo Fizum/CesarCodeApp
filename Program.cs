47d547c51eed459b830d758893e68767c208f187
studenti <studenti@rosselli.loc>
2020-11-20 08:46:13 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CesarCodeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = 0;
+            string plainText = "";
+
+            Console.WriteLine("inserisci chiave");
+            key = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("inserisci testo");
+            plainText = Console.ReadLine();
+
+
+        }
+    }
+}
